beb71b4f5f6031798e2b3bb4c5e4618f6a3eeb74
Dylan <dylancychua@gmail.com>
2022-01-18 12:59:37 +0800

    Commit 1.44

--- server.js
+++ server.js
@@ -90,6 +90,7 @@ app.post('/register', checkNotAuthenticated, async (req, res) => {
       id: Date.now().toString(),
       name: req.body.name,
       email: req.body.email,
+      position: req.body.position,
       password: hashedPassword
     })
     res.redirect('/login')
